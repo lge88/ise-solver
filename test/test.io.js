c4492d6ab23714c0468c0237311da82bf3d19cb7
Li Ge <lge@ucsd.edu>
2013-10-16 08:37:42 -0700

    add io server

--- test/test.io.js
+++ test/test.io.js
@@ -1,23 +1,43 @@
+var expect = require( 'expect.js' );
 
-// var ISESolverServer = require( 'ise-solver' ).Server;
-// var ISESolverClient = require( 'ise-solver' ).Client;
-// var server = new ISESolverServer( { port: 8080 } );
-// server.listen();
+var ISESolverIOServer = require( 'ise-solver' ).io.Server;
+var ISESolverIOClient = require( 'ise-solver' ).io.Client;
+var port = 3000;
+var ns = '/solve';
+var io = require( 'socket.io' )
+  .listen( port )
+  .of( ns );
 
-// var socket = require( 'socket.io-client' )( 'ws://vision2.ucsd.edu:8080' );
-// var solverClient = new ISESolverClient( { socket: socket } );
+var server = new ISESolverIOServer( { io: io } );
+var socket = require( 'socket.io-client' )
+  .connect( 'http://localhost:' + port + ns );
+var solverClient = new ISESolverIOClient( socket );
 
-// var expect = require( 'expect.js' );
+describe( 'ise-solver', function() {
 
-// describe( 'ise-solver', function() {
+  it( 'should output correct nodal disp', function( done ) {
+    // var truesses = require( './fixtures/trusses' );
 
-//   it( 'should return Hello World', function( done ) {
-//     var model = require( './fixtures/trusses' );
-//     solverClient
-//       .model( model )
-//       .on( 'data',  )
-//       .solve(  )
-//     expect( solverClient ).to.be( 'Hello World' );
-//   } );
+    // solverClient
+    //   .model( trusses )
+    //   .analysisType( 'static' )
+    //   .boilerplate()
+    //   .addRecorder({ type: 'NodalDisplacement' })
+    //   .analyze( 10 )
+    //   .then(function( model ) {
+    //     var x = 0.53009277713228375450;
+    //     var y = -0.17789363846931768864;
+    //     var state = model.state;
+    //     expect( state.nodalDisplacements[ 4 ][ 0 ] - x ).to.be.within( -1e-8, 1e-8 );
+    //     expect( state.nodalDisplacements[ 4 ][ 1 ] - y ).to.be.within( -1e-8, 1e-8 );
+    //     expect( state.nodalDisplacements[ 3 ][ 1 ] - y ).to.not.be.within( -1e-8, 1e-8 );
+    //   }, function( err ) {
+    //     // console.log( err );
+    //   }, function( state ) {
+    //     // console.log( 'state', state );
+    //   })
+    //   .then( done, done );
 
-// } );
+  } );
+
+} );
